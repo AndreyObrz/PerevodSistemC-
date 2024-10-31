ba122c9a17d043e2e2101eb049d65dde694b305d
Дом <@DESKTOP-DF6QSIM>
2024-10-31 22:23:51 +0300

    Добавьте файлы проекта.

---
+++ PerevodSistemC++/PerevodSistemC++.cpp
@@ -0,0 +1,17 @@
+﻿#include <cstdlib>
+#include <iostream>
+#include <windows.h>
+#include <cmath>
+#include <math.h>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    
+
+    system("pause");
+    return EXIT_SUCCESS;
+}
+
