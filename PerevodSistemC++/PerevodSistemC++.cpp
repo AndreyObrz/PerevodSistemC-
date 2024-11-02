6802c6342b7b2f148782bf4bca9aaff706d08ddc
MoonIParadizo <obrezkov.andrej@list.ru>
2024-11-02 12:30:55 +0300

    Считывание строки, подсчет элементов.

--- PerevodSistemC++/PerevodSistemC++.cpp
+++ PerevodSistemC++/PerevodSistemC++.cpp
@@ -9,7 +9,23 @@ int main(int argc, char* argv[])
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    
+    int counts = 0;
+
+    double d  = -1;
+    char s[100];
+    cin.getline(s, 100);
+    int i = 0;
+    double f;
+    cin >> f;
+    while (s[i] != '\0') {
+        if (s[i] == '.') {
+            counts = 1;
+        }
+        if (counts == 0) {
+            d++;
+        }
+        i++;
+    }
 
     system("pause");
     return EXIT_SUCCESS;
