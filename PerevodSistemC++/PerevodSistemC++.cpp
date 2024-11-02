d827e50be7df5fd613dfe317e3f4add8358ae98d
MoonIParadizo <obrezkov.andrej@list.ru>
2024-11-02 12:36:16 +0300

    Запись в summa1-2, переведенных в десятичную

--- PerevodSistemC++/PerevodSistemC++.cpp
+++ PerevodSistemC++/PerevodSistemC++.cpp
@@ -26,6 +26,39 @@ int main(int argc, char* argv[])
         }
         i++;
     }
+    int summa1 = 0;
+    double summa2 = 0;
+    counts = 0;
+    i = 0;
+    while (s[i] != '\0') {
+        if (s[i] == '.') { counts = 1; }
+        else {
+            if (counts == 0) {
+                if ((int)s[i] - 55 >= 10) {
+                    summa1 = summa1 + ((int)s[i] - 55) * pow(f, d);
+                    d--;
+                }
+                else {
+                    summa1 = summa1 + ((int)s[i] - 48) * pow(f, d);
+                    d--;
+                }
+            }
+
+            if (counts == 1) {
+                if ((int)s[i] - 55 >= 10) {
+                    summa2 = summa2 + ((int)s[i] - 55) * pow(f, d);
+                    d--;
+                }
+                else {
+                    summa2 = summa2 + ((int)s[i] - 48) * pow(f, d);
+                    d--;
+                }
+            }
+        }
+        i++;
+    }
+
+
 
     system("pause");
     return EXIT_SUCCESS;
